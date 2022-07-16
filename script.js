fad3f1bde18178afc8b09258d8898ac0500da89b
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2022-07-16 17:46:45 +0530

    Bible Box 1
    box

---
+++ script.js
@@ -0,0 +1,26 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js'); 
+} 
